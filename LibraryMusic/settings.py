987823aa35a5b6a5d1e3f0eed329cc65683a2198
JenriquePS <jenrique.ps@gmail.com>
2021-01-07 23:33:30 -0500

    add db and config vars

--- LibraryMusic/settings.py
+++ LibraryMusic/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -20,8 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w&#uqhdki63_vi2vki7ft^r1a4@@vf51qvb#skgysy52j26yi-'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -75,10 +76,12 @@ WSGI_APPLICATION = 'LibraryMusic.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(
+        os.getenv(
+            'DATABASE_URL',
+            'sqlite:///{}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+        )
+    )
 }
 
 
